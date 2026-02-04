6dfe096a02f8e6015ac64496b96ddafbd9cd1253
ka1dyn <wt0630@naver.com>
2026-02-04 14:49:17 +0900

    🐛 Set console debug for git config

--- vercel-prebuild.sh
+++ vercel-prebuild.sh
@@ -1,5 +1,7 @@
 git config --local url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
 
+git config --local --list
+
 git submodule sync
 
 git submodule update --init --recursive
