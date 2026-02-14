8788e55321a9c828ab99e1a2f25b342f52a47880
ka1dyn <wt0630@naver.com>
2026-02-14 22:42:13 +0900

    💄 UI rollback

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -29,7 +29,7 @@ export default function Category({ category, className }: CategoryProps) {
   return (
     <div
       className={cn(
-        "px-2 py-0.5 bolder text-sm rounded-md w-fit border",
+        "px-2 py-1 bolder text-xs rounded-md w-fit border",
         className,
       )}
       style={{
