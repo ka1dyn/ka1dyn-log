be54dadf4fd05950eae746960a2aabee4b53547a
ka1dyn <wt0630@naver.com>
2026-03-07 06:18:58 +0900

    🐛 Add trace console

--- vercel-prebuild.sh
+++ vercel-prebuild.sh
@@ -1,4 +1,4 @@
-git config --global url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
+GIT_TRACE=1 git config --global url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
 
 git config --global --list
 
