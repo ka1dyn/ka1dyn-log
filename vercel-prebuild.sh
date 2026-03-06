f45e9a6151220382547df63d96416b173e359695
ka1dyn <wt0630@naver.com>
2026-03-07 05:56:31 +0900

    🐛 Console test

--- vercel-prebuild.sh
+++ vercel-prebuild.sh
@@ -1,5 +1,7 @@
 git config --global url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
 
+git config --global --list
+
 git submodule sync
 
 git submodule update --init --recursive
